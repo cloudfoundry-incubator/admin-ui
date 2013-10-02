2c0af3899203cfdc0534f1aab102b09a5b66aec4
J Russell Boykin <rboykin@us.ibm.com>
2013-10-02 07:33:51 -0500

    Add rspec unit testing for statistics

--- spec/unit/admin_spec.rb
+++ spec/unit/admin_spec.rb
@@ -14,25 +14,30 @@ ADMIN_PASSWORD = 'admin_passw0rd'
 USER          = 'user'
 USER_PASSWORD = 'user_passw0rd'
 
+CLOUD_CONTROLLER_URI = 'http://api.localhost'
+
+TASKS_REFRESH_INTERVAL = 6000
+
 
 describe Admin do
   before(:all) do
     config = 
     {
-      :cc                   => 'postgres://ccadmin:c1oudc0w@localhost:5524/ccdb',
-      :cloud_controller_uri => 'http://api.localhost',
-      :data_file            => DATA_FILE,
-      :log_file             => LOG_FILE,
-      :log_files            => [],
-      :mbus                 => 'nats://nats:c1oudc0w@localhost:4222',
-      :monitored_components => ['ALL'],
-      :port                 => PORT,
-      :receiver_emails      => [],
-      :sender_email         => {:server => 'localhost', :account => 'system@localhost'},
-      :stats_file           => STATS_FILE,
-      :uaa                  => 'postgres://uaaadmin:c1oudc0w@localhost:5524/uaadb',
-      :ui_admin_credentials => {:username => ADMIN_USER, :password => ADMIN_PASSWORD},
-      :ui_credentials       => {:username => USER, :password => USER_PASSWORD}
+      :cc                     => 'postgres://ccadmin:c1oudc0w@localhost:5524/ccdb',
+      :cloud_controller_uri   => CLOUD_CONTROLLER_URI,
+      :data_file              => DATA_FILE,
+      :log_file               => LOG_FILE,
+      :log_files              => [],
+      :mbus                   => 'nats://nats:c1oudc0w@localhost:4222',
+      :monitored_components   => ['ALL'],
+      :port                   => PORT,
+      :receiver_emails        => [],
+      :sender_email           => {:server => 'localhost', :account => 'system@localhost'},
+      :stats_file             => STATS_FILE,
+      :tasks_refresh_interval => TASKS_REFRESH_INTERVAL,
+      :uaa                    => 'postgres://uaaadmin:c1oudc0w@localhost:5524/uaadb',
+      :ui_admin_credentials   => {:username => ADMIN_USER, :password => ADMIN_PASSWORD},
+      :ui_credentials         => {:username => USER, :password => USER_PASSWORD}
     }
 
     File.open(CONFIG_FILE, 'w') { |file| file.write(JSON.pretty_generate(config)) }
@@ -53,10 +58,40 @@ describe Admin do
     Process.wait(cleanup_files_pid)
   end
 
+  def create_http
+    @http = Net::HTTP.new(HOST, PORT)
+  end
+
+  def clear_http
+    @http = nil
+  end
+
+  def login
+    create_http
+
+    request = Net::HTTP::Post.new("/login?username=#{ADMIN_USER}&password=#{ADMIN_PASSWORD}")
+    request['Content-Length'] = 0
+
+    response = @http.request(request)
+    fail_with('Unexpected http status code') unless response.is_a?(Net::HTTPSeeOther)
+
+    location = response['location']
+    location.should eq("http://#{HOST}:#{PORT}/application.html?user=#{ADMIN_USER}")
+
+    @cookie = response['Set-Cookie']
+    @cookie.should_not be_nil
+  end 
+
+  def logout
+    clear_http
+
+    @cookie = nil
+  end
+
   context 'Login required, performed and failed' do
     
     it 'login fails as expected' do
-      @http   = Net::HTTP.new(HOST, PORT)
+      create_http
       request = Net::HTTP::Post.new("/login?username=#{ADMIN_USER}&password=#{USER_PASSWORD}")
       request['Content-Length'] = 0
 
@@ -71,23 +106,11 @@ describe Admin do
   context 'Login required, performed and succeeded' do
 
     before(:all) do
-      @http   = Net::HTTP.new(HOST, PORT)
-      request = Net::HTTP::Post.new("/login?username=#{ADMIN_USER}&password=#{ADMIN_PASSWORD}")
-      request['Content-Length'] = 0
-
-      response = @http.request(request)
-      fail_with('Unexpected http status code') unless response.is_a?(Net::HTTPSeeOther)
-
-      location = response['location']
-      location.should eq("http://#{HOST}:#{PORT}/application.html?user=#{ADMIN_USER}")
-
-      @cookie = response['Set-Cookie']
-      @cookie.should_not be_nil
+      login
     end
 
     after(:all) do
-      @http   = nil
-      @cookie = nil
+      logout
     end
 
     def get_json(path)
@@ -103,36 +126,41 @@ describe Admin do
       JSON.parse(body)
     end
 
+    def verify_not_connected_empty_items(path)
+      json = get_json(path)
+
+      json.should eq({'connected' => false, 
+                      'items'     => []})
+    end
+
     def verify_empty_items(path)
       json = get_json(path)
 
-      items = json['items']
-      items.should_not be_nil
-      items.length.should eq(0)
+      json.should eq({'items' => []})
     end
 
     it '/applications succeeds' do
-      verify_empty_items('/applications')
+      verify_not_connected_empty_items('/applications')
     end
 
     it '/cloudControllers succeeds' do
-      verify_empty_items('/cloudControllers')
+      verify_not_connected_empty_items('/cloudControllers')
     end
 
     it '/components succeeds' do
-      verify_empty_items('/components')
+      verify_not_connected_empty_items('/components')
     end
 
     it '/dropletExecutionAgents succeeds' do
-      verify_empty_items('/dropletExecutionAgents')
+      verify_not_connected_empty_items('/dropletExecutionAgents')
     end
 
     it '/gateways succeeds' do
-      verify_empty_items('/gateways')
+      verify_not_connected_empty_items('/gateways')
     end
 
     it '/healthManagers succeeds' do
-      verify_empty_items('/healthManagers')
+      verify_not_connected_empty_items('/healthManagers')
     end
 
     it '/logs succeeds' do
@@ -140,23 +168,24 @@ describe Admin do
     end
 
     it '/organizations succeeds' do
-      verify_empty_items('/organizations')
+      verify_not_connected_empty_items('/organizations')
     end
 
     it '/routers succeeds' do
-      verify_empty_items('/routers')
+      verify_not_connected_empty_items('/routers')
     end
 
     it '/settings succeeds' do
       json = get_json('/settings')
 
-      json['cloudControllerURI'].should_not be_nil
-      json['tasksRefreshInterval'].should_not be_nil
-      json['admin'].should_not be_nil
+      json.should eq({'cloudControllerURI'   => CLOUD_CONTROLLER_URI,
+                      'tasksRefreshInterval' => TASKS_REFRESH_INTERVAL,
+                      'admin'                => true})
+
     end
 
     it '/spaces succeeds' do
-      verify_empty_items('/spaces')
+      verify_not_connected_empty_items('/spaces')
     end
 
     it '/tasks succeeds' do
@@ -164,18 +193,19 @@ describe Admin do
     end
 
     it '/users succeeds' do
-      verify_empty_items('/users')
+      verify_not_connected_empty_items('/users')
     end
+
   end
 
   context 'Login required, but not performed' do
 
     before(:all) do
-      @http = Net::HTTP.new(HOST, PORT)
+      create_http
     end
 
     after(:all) do
-      @http = nil
+      clear_http
     end
 
     def redirects_as_expected(path)
@@ -245,11 +275,11 @@ describe Admin do
   context 'Login not required' do
 
     before(:all) do
-      @http = Net::HTTP.new(HOST, PORT)
+      create_http
     end
 
     after(:all) do
-      @http = nil
+      clear_http
     end
 
    def get_response(path)
@@ -270,12 +300,6 @@ describe Admin do
       body
     end
 
-    def get_json(path)
-      body = get_body(path)
-
-      JSON.parse(body)
-    end
-
     it '/ succeeds' do
       get_body('/')
     end
@@ -284,20 +308,65 @@ describe Admin do
       get_response('/favicon.ico')
     end
 
-    it '/statistics succeeds' do
-      json = get_json('/statistics')
-
-      json['label'].should_not be_nil
-
-      items = json['items']
-      items.should_not be_nil
-      items.length.should eq(0)
-    end
-
     it '/stats succeeds' do
       get_body('/stats')
     end
 
+  end
+
+  context 'Statistics' do
+      
+    it '/statistics post succeeds' do
+      login
+
+      begin
+        request = Net::HTTP::Post.new('/statistics?timestamp=1&users=2&apps=3&running_instances=4&total_instances=5&deas=6')
+        request['Cookie']         = @cookie
+        request['Content-Length'] = 0
+
+        response = @http.request(request)
+        fail_with('Unexpected http status code') unless response.is_a?(Net::HTTPOK)
+
+        body = response.body
+        body.should_not be_nil
+     
+        json = JSON.parse(body)
+        json.should eq({'timestamp'         => 1, 
+                        'users'             => 2, 
+                        'apps'              => 3, 
+                        'running_instances' => 4, 
+                        'total_instances'   => 5, 
+                        'deas'              => 6})
+      ensure
+        logout
+      end
+    end
+
+    it '/statistics succeeds' do
+      create_http
+
+      begin
+        request = Net::HTTP::Get.new('/statistics')
+
+        response = @http.request(request)
+        fail_with('Unexpected http status code') unless response.is_a?(Net::HTTPOK)
+
+        body = response.body
+        body.should_not be_nil
+
+        json = JSON.parse(body)
+      
+        json.should eq({'label' => CLOUD_CONTROLLER_URI, 
+                        'items' => [{'timestamp'         => 1, 
+                                     'users'             => 2, 
+                                     'apps'              => 3, 
+                                     'running_instances' => 4, 
+                                     'total_instances'   => 5, 
+                                     'deas'              => 6}]})
+      ensure
+        clear_http
+      end
+    end
   end
 
 end
