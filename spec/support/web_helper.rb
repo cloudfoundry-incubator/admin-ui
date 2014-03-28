5523a472e81030fb2b55f8587ad5e0f94b6b047f
Doug Davis <dug@us.ibm.com>
2014-03-27 22:37:36 -0400

    add saucelab stuff from tom

--- spec/support/web_helper.rb
+++ spec/support/web_helper.rb
@@ -12,14 +12,31 @@ end
 shared_context :web_context do
   let(:stat_date) { 1_383_238_113_597 }
   let(:stat_count) { 1 }
-
   before do
-    @driver = Selenium::WebDriver.for :firefox
+    @driver = selenium_web_driver
     @driver.manage.timeouts.implicit_wait = 5
   end
 
   after do
     @driver.quit
+  end
+
+  def selenium_web_driver
+    return  Selenium::WebDriver.for(:firefox) unless ENV['TRAVIS']
+
+    access_key        = ENV['SAUCE_ACCESS_KEY']
+    build_number      = ENV['TRAVIS_BUILD_NUMBER']
+    tunnel_identifier = ENV['TRAVIS_JOB_NUMBER']
+    username          = ENV['SAUCE_USERNAME']
+
+    caps = Selenium::WebDriver::Remote::Capabilities.new(
+        :browser_name       => 'firefox',
+        :build              => build_number,
+        'tunnel-identifier' => tunnel_identifier)
+
+    url = "http://#{ username }:#{ access_key }@localhost:4445/wd/hub"
+
+    Selenium::WebDriver.for(:remote, :desired_capabilities => caps, :url => url)
   end
 
   def add_stats
