c2c5a731978fa3ee9f42a508fe16f29176bdfdb4
J Russell Boykin <rboykin@us.ibm.com>
2015-06-03 07:09:38 -0500

    Multi-column, server-side sorting

--- lib/admin/view_models/sort_action.rb
+++ lib/admin/view_models/sort_action.rb
@@ -3,58 +3,74 @@ require_relative 'base_action'
 module AdminUI
   class SortAction < AdminUI::BaseAction
     def do_items
-      i_sorting_cols = @params[:iSortingCols].to_i
+      sort_columns = SortColumn.create_array_from_params(@source, @params)
 
-      # TODO: Currently limiting to a single column sort since that is all the UI side is currently supporting
-      return @source unless i_sorting_cols == 1
-
-      i_sort_col = @params['iSortCol_0'].to_i
-      s_sort_dir = @params['sSortDir_0']
+      return @source if sort_columns.length == 0
 
       sorted = @source[:items]
-      if @source[:case_insensitive_sort_columns].include?(i_sort_col)
-        if s_sort_dir == 'asc'
-          sorted = sorted.sort do |a, b|
-            a_col = a[i_sort_col]
-            b_col = b[i_sort_col]
-            next a_col.to_s.casecmp(b_col.to_s) if a_col && b_col
-            next 1 if a_col
-            next -1 if b_col
-            next 0
+      sorted = sorted.sort do |a_row, b_row|
+        # Start row compare at 0. Any difference among selected columns will terminate the inner loop
+        compare = 0
+
+        sort_columns.each do |sort_column|
+          column   = sort_column.column
+          a_column = a_row[column]
+          b_column = b_row[column]
+
+          if a_column
+            if b_column
+              if sort_column.case_insensitive
+                compare = a_column.to_s.casecmp(b_column.to_s)
+              else
+                compare = a_column <=> b_column
+              end
+            else
+              compare = 1
+            end
+          elsif b_column
+            compare = -1
           end
-        else
-          sorted = sorted.sort do |a, b|
-            a_col = a[i_sort_col]
-            b_col = b[i_sort_col]
-            next b_col.to_s.casecmp(a_col.to_s) if a_col && b_col
-            next 1 if b_col
-            next -1 if a_col
-            next 0
-          end
+
+          compare = -compare unless sort_column.ascending
+
+          # Terminate looping if everything does not compare.  Two nil columns considered as equal.
+          break unless compare == 0
         end
-      else
-        if s_sort_dir == 'asc'
-          sorted = sorted.sort do |a, b|
-            a_col = a[i_sort_col]
-            b_col = b[i_sort_col]
-            next a_col <=> b_col if a_col && b_col
-            next 1 if a_col
-            next -1 if b_col
-            next 0
-          end
-        else
-          sorted = sorted.sort do |a, b|
-            a_col = a[i_sort_col]
-            b_col = b[i_sort_col]
-            next b_col <=> a_col if a_col && b_col
-            next 1 if b_col
-            next -1 if a_col
-            next 0
-          end
-        end
+
+        next compare
       end
 
       result(@source[:connected], sorted, @source[:detail_hash], @source[:searchable_columns], @source[:case_insensitive_sort_columns])
+    end
+  end
+
+  class SortColumn
+    attr_reader :column, :ascending, :case_insensitive
+
+    def initialize(column, ascending, case_insensitive)
+      @column           = column
+      @ascending        = ascending
+      @case_insensitive = case_insensitive
+    end
+
+    def self.create_array_from_params(source, params)
+      case_insensitive_sort_columns = source[:case_insensitive_sort_columns]
+      i_sorting_cols                = params[:iSortingCols].to_i
+
+      result = []
+
+      index = 0
+      while index < i_sorting_cols
+        i_sort_col                   = params["iSortCol_#{ index }"].to_i
+        s_sort_dir                   = params["sSortDir_#{ index }"]
+        case_insensitive_sort_column = case_insensitive_sort_columns.include?(i_sort_col)
+
+        result.push(SortColumn.new(i_sort_col, s_sort_dir == 'asc', case_insensitive_sort_column))
+
+        index += 1
+      end
+
+      result
     end
   end
 end
